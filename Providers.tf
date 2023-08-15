b955d1d7542a569d16cc84f1508cc65b4a228566
Dolapo Fashina <dolapofashina@gmail.com>
2023-08-15 09:17:59 +0100

    Update Providers.tf

--- Providers.tf
+++ Providers.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+  
+# configure Aws provider 
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
