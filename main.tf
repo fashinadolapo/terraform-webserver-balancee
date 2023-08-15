9cb1031cfd694517dd11c1c09921b2d37c5ad8b3
Dolapo Fashina <dolapofashina@gmail.com>
2023-08-15 09:46:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+# create ec2 instance 
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Name = "balancee-server-${terraform.workspace}"
+  }
+}
 
