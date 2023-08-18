80647a11c5f72f577fc6f289a2fea1118b76ae27
Dolapo Fashina <dolapofashina@gmail.com>
2023-08-18 11:15:07 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+yum update -y
+amazon-linux-extras install nginx1.12 -y
+service nginx start
+echo "<html><body><h1>Hello World!</h1></body></html>" > /usr/share/nginx/html/index.html
