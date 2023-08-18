f6bdcffcb46dff84da9bb1f7eaeb76a19746cd8c
Dolapo Fashina <dolapofashina@gmail.com>
2023-08-18 11:07:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "vpc" {
 module "key_pair" {
   source = "terraform-aws-modules/key-pair/aws"
 
-  key_name           = var.key_pair
+  key_name           = var.key_name
   create_private_key = true
 }
 
